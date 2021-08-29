3a485055ebc9388277213e4cfd2a02f4de4df234
Ganesan Deepak Guru <deepak.guru.ganesan@scania.com>
2021-08-29 13:02:14 +0200

    Create Solution.py

---
+++ 10 Days of Statistics/Day 0 - Weighted Mean/Solution.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedMean' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY X
+#  2. INTEGER_ARRAY W
+#
+
+def weightedMean(X, W):
+    ws = 0
+    s=0
+    for i,j in zip(X,W):
+        ws += i*j
+        s += j
+    wm = "{:.1f}".format(ws/s)   
+    print(wm)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    vals = list(map(int, input().rstrip().split()))
+
+    weights = list(map(int, input().rstrip().split()))
+
+    weightedMean(vals, weights)
